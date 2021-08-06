92d7453cf03ebfca2a425f88d0a3ece1f5edb393
Mohammad Ullah <mohammad.cse2290@gmail.com>
2021-08-07 02:51:11 +0600

    template and multiline practice

---
+++ template.js
@@ -0,0 +1,16 @@
+const firstName = "Justin";
+const lastName = "Trudo";
+const fullName = firstName +" " + lastName;
+
+const fullName2 = `${firstName} ${20+20} is a good boy`;
+console.log(fullName2);
+
+const multiline = "I love you\n"
+    + "I miss you\n"
+    + "I need you\n";
+console.log(multiline);
+
+const multiline2 = `I love you
+I miss you
+Ekhn er baily road a dorker nai.`;
+console.log(multiline2);
