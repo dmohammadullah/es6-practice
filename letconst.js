958587db6fde1f354ff29944862d59c5d5caa2d8
Mohammad Ullah <mohammad.cse2290@gmail.com>
2021-08-07 00:33:38 +0600

    2nd commit

---
+++ letconst.js
@@ -0,0 +1,22 @@
+const hubby = "rony";
+//hubby="omor sunny";
+console.log(hubby);
+
+const numbers = [12, 23, 24];
+numbers[1]= 88;
+numbers.push(12);
+numbers.pop();
+console.log(numbers);
+
+const nayok = {name:"sakib", phone:1546};
+console.log(nayok);
+
+let patientName = "Rahim chacha";
+patientName = "Fatema khala";
+console.log(patientName);
+
+let sum = 0;
+for(let i = 0; i < 10; i++){
+    sum = sum +i;
+}
+console.log(i);
